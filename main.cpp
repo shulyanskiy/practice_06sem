89eb13938b589bd76165ff5c28338fcb718d1786
hik0N <138707883+shulyanskiy@users.noreply.github.com>
2026-07-09 10:10:31 +0300

    upload servers main.cpp

--- main.cpp
+++ main.cpp
@@ -1,66 +1,21 @@
+#include "include/ServerWidget.h"
 #include <QApplication>
-#include <QThread>
-#include "include/AntennaClient.h"
-#include "include/MyAntenn.h"
-#include "include/emulator.h"
+#include <QStyleFactory>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    // Создаем объекты
-    MyAntenn* antenn = new MyAntenn();
-    EmuManager* manager = new EmuManager(antenn);
+    app.setApplicationName("Antenna Server");
+    app.setOrganizationName("AntennaEmulator");
 
-    // Создаем отдельные потоки
-    QThread* antennaThread = new QThread();
-    QThread* serverThread = new QThread();
+    // Устанавливаем стиль
+    app.setStyle(QStyleFactory::create("Fusion"));
 
-    // Перемещаем объекты в их потоки
-    antenn->moveToThread(antennaThread);
-    manager->moveToThread(serverThread);
+    ServerWidget server;
+    server.setWindowTitle("Antenna Emulator Server");
+    server.resize(1100, 800);
+    server.show();
 
-    // Запуск потоков
-    QObject::connect(antennaThread, &QThread::started, [antenn]() {
-        antenn->start();
-        });
-
-    QObject::connect(serverThread, &QThread::started, [manager]() {
-        manager->start(12345);
-        });
-
-    // Остановка потоков
-    QObject::connect(serverThread, &QThread::finished, [manager]() {
-        manager->stop();
-        });
-
-    QObject::connect(antennaThread, &QThread::finished, [antenn]() {
-        antenn->stop();
-        });
-
-    // Запускаем потоки
-    antennaThread->start();
-    serverThread->start();
-
-    // Даем время на инициализацию
-    QThread::msleep(100);
-
-    // Запускаем клиент в главном потоке
-    AntennaClient client;
-    client.show();
-
-    int result = app.exec();
-
-    // Останавливаем потоки
-    serverThread->quit();
-    serverThread->wait();
-    antennaThread->quit();
-    antennaThread->wait();
-
-    delete serverThread;
-    delete antennaThread;
-    delete manager;
-    delete antenn;
-
-    return result;
+    return app.exec();
 }
